30be5f4b5c57aa515fb8dda0c46e7f180c8d155d
mmo <362651591@qq.com>
2015-10-14 22:54:28 +0800

    safsdf

--- script/lualib/stream.lua
+++ script/lualib/stream.lua
@@ -14,6 +14,11 @@ local TYPE_FLOAT = 10
 local TYPE_DOUBLE = 11
 local TYPE_STRING = 12
 
+local tinsert = table.insert
+local tunpack = table.unpack
+local setmetatable = setmetatable
+local coreWrite = writerCore.Write
+local coreRead = readerCore.Read
 
 local _M = {}
 
@@ -24,71 +29,71 @@ function _M:New()
 end
 
 function _M:WriteBool(v)
-	table.insert(self.data,TYPE_BOOL)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_BOOL)
+	tinsert(self.data,v)
 end
 
 function _M:WriteUInt8(v)
-	table.insert(self.data,TYPE_UINT8)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_UINT8)
+	tinsert(self.data,v)
 end
 
 function _M:WriteUInt16(v)
-	table.insert(self.data,TYPE_UINT16)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_UINT16)
+	tinsert(self.data,v)
 end
 
 function _M:WriteUInt32(v)
-	table.insert(self.data,TYPE_UINT32)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_UINT32)
+	tinsert(self.data,v)
 end
 
 function _M:WriteUInt64(v)
-	table.insert(self.data,TYPE_UINT64)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_UINT64)
+	tinsert(self.data,v)
 end
 
 function _M:WriteInt8(v)
-	table.insert(self.data,TYPE_INT8)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_INT8)
+	tinsert(self.data,v)
 end
 
 function _M:WriteInt16(v)
-	table.insert(self.data,TYPE_INT16)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_INT16)
+	tinsert(self.data,v)
 end
 
 function _M:WriteInt32(v)
-	table.insert(self.data,TYPE_INT32)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_INT32)
+	tinsert(self.data,v)
 end
 
 function _M:WriteInt64(v)
-	table.insert(self.data,TYPE_INT64)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_INT64)
+	tinsert(self.data,v)
 end
 
 function _M:WriteFloat(v)
-	table.insert(self.data,TYPE_FLOAT)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_FLOAT)
+	tinsert(self.data,v)
 end
 
 function _M:WriteDouble(v)
-	table.insert(self.data,TYPE_DOUBLE)
-	table.insert(self.data,v)
+	tinsert(self.data,TYPE_DOUBLE)
+	tinsert(self.data,v)
 end
 
 function _M:WriteString(str)
-	table.insert(self.data,TYPE_STRING)
-	table.insert(self.data,str)
+	tinsert(self.data,TYPE_STRING)
+	tinsert(self.data,str)
 end
 
 function _M:Pack()
-	return writerCore.Write(table.unpack(self.data))
+	return coreWrite(tunpack(self.data))
 end
 
 function _M.Unpack(...)
-	return readerCore.Read(...)
+	return coreRead(...)
 end
 
 return _M
