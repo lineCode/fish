0b3287c2fedd26f82ad4df5c4107eb93d3824429
mmo <362651591@qq.com>
2015-10-18 17:02:29 +0800

    zasdf

--- script/test/test_telnet.lua
+++ script/test/test_telnet.lua
@@ -8,10 +8,14 @@ function cmd.test(...)
 	print(...)
 end
 
+function cmd.stop()
+	fish.Stop()
+end
+
 fish.Start(function ()
 	fish.Log("test telnet")
 
 	telnet.Start("127.0.0.1",1990,cmd)
-	
+
 end)
 
