94159d120390137482bce4931bc56183e0cc9dfb
mmo <362651591@qq.com>
2015-12-05 13:34:12 +0800

    asdfasdf

--- lib/core/util/PoolObject.h
+++ lib/core/util/PoolObject.h
@@ -3,73 +3,85 @@
 #include "../Typedef.h"
 #include "../thread/Mutex.h"
 
+struct Pool
+{
+	struct Pool* _next;
+};
+
 template <class TOBJECT, int ALLOC_COUNT=10>
 class PoolObject
 {
 public:
-
 	static void* operator new(size_t objSize)
 	{
-		mMutex.lock();
+		_mutex.lock();
 
-		if (!mFreeList)
+		if (!_freelist)
 		{
-			mFreeList = new uint8[sizeof(TOBJECT)*ALLOC_COUNT] ;
+			Pool* pool = (Pool*)malloc(sizeof(Pool) + sizeof(TOBJECT)*ALLOC_COUNT);
+			pool->_next = _pool;
+			_pool = pool;
+			
+			_freelist = (uint8*)((Pool*)pool + 1);
 
-			uint8* pNext = mFreeList ;
-			uint8** ppCurr = reinterpret_cast<uint8**>(mFreeList) ;
+			uint8* next = _freelist ;
+			uint8** curr = reinterpret_cast<uint8**>(_freelist) ;
 
 			for (int i=0 ; i<ALLOC_COUNT-1 ; ++i)
 			{
-				pNext += sizeof(TOBJECT) ;
-				*ppCurr = pNext ;
-				ppCurr = reinterpret_cast<uint8**>(pNext) ;
+				next += sizeof(TOBJECT) ;
+				*curr = next ;
+				curr = reinterpret_cast<uint8**>(next) ;
 			}
 
-			*ppCurr = 0 ;
-			mTotalAllocCount += ALLOC_COUNT ;
+			*curr = 0 ;
+			_allocCount += ALLOC_COUNT ;
 		}
 
-		uint8* pAvailable = mFreeList ;
-		mFreeList = *reinterpret_cast<uint8**>(pAvailable) ;
-		++mCurrentUseCount ;
+		uint8* result = _freelist ;
+		_freelist = *reinterpret_cast<uint8**>(result) ;
+		++_useCount ;
 
-		mMutex.unlock();
+		_mutex.unlock();
 
-		return pAvailable ;
+		return result;
 	}
 
 	static void	operator delete(void* obj)
 	{
-		mMutex.lock();
+		_mutex.lock();
 
-		--mCurrentUseCount ;
+		--_useCount ;
 
-		*reinterpret_cast<uint8**>(obj) = mFreeList ;
-		mFreeList = static_cast<uint8*>(obj) ;
+		*reinterpret_cast<uint8**>(obj) = _freelist ;
+		_freelist = static_cast<uint8*>(obj) ;
 
-		mMutex.unlock();
+		_mutex.unlock();
 	}
 
 
 private:
-	static uint8*		 mFreeList ;
-	static int			 mTotalAllocCount ;
-	static int			 mCurrentUseCount ;
-	static Thread::Mutex mMutex;
+	static uint8*		 _freelist;
+	static Pool*		 _pool;
+	static int			 _allocCount ;
+	static int			 _useCount ;
+	static Thread::Mutex _mutex;
 };
 
 
 template <class TOBJECT, int ALLOC_COUNT>
-uint8* PoolObject<TOBJECT, ALLOC_COUNT>::mFreeList = NULL ;
+uint8* PoolObject<TOBJECT, ALLOC_COUNT>::_freelist = NULL ;
 
 template <class TOBJECT, int ALLOC_COUNT>
-int PoolObject<TOBJECT, ALLOC_COUNT>::mTotalAllocCount = 0 ;
+Pool* PoolObject<TOBJECT, ALLOC_COUNT>::_pool = NULL ;
 
 template <class TOBJECT, int ALLOC_COUNT>
-int PoolObject<TOBJECT, ALLOC_COUNT>::mCurrentUseCount = 0 ;
+int PoolObject<TOBJECT, ALLOC_COUNT>::_allocCount = 0 ;
 
 template <class TOBJECT, int ALLOC_COUNT>
-Thread::Mutex PoolObject<TOBJECT, ALLOC_COUNT>::mMutex;
+int PoolObject<TOBJECT, ALLOC_COUNT>::_useCount = 0 ;
+
+template <class TOBJECT, int ALLOC_COUNT>
+Thread::Mutex PoolObject<TOBJECT, ALLOC_COUNT>::_mutex;
 
 #endif
