7a3e1f11daccc66722779af8c659545c8cc349e7
2109 <cai32109@gmail.com>
2018-12-25 19:36:51 +0800

    bug

---
+++ hx/thread/Thread.h
@@ -0,0 +1,55 @@
+﻿#ifndef THEAD_H
+#define THEAD_H
+
+#if defined(WIN32)
+#include <winsock2.h>
+#include <windows.h> 
+#endif
+
+#include "../Typedef.h"
+
+namespace Thread 
+{
+	class WorkerThread
+	{
+	public:
+		WorkerThread();
+		~WorkerThread();
+
+		void Start();
+		bool Join();
+
+		virtual void Run() = 0;
+
+		virtual void Wait();
+		virtual void Wakeup();
+
+		virtual void initCond(void);
+
+		virtual void initMutex(void);
+
+		virtual void deleteCond(void);
+
+		virtual void deleteMutex(void);
+
+		virtual void lock(void);
+
+		virtual void unlock(void);	
+
+#if defined( WIN32 )
+		static unsigned __stdcall threadFunc(void *ctx);
+#else
+		static void*			  threadFunc(void* ctx);
+#endif
+
+	protected:
+		THREAD_ID		_threadId;
+		THREAD_SINGNAL  _cond;		
+		THREAD_MUTEX	_mutex;		
+	};
+
+
+	void Sleep(int wait);
+}
+
+#endif
---
+++ server/Bootstrap.h
@@ -0,0 +1,21 @@
+﻿#ifndef BOOTSTRAP_H
+#define BOOTSTRAP_H
+#include "reader.h"
+#include "document.h"  
+
+class Bootstrap
+{
+public:
+	Bootstrap(void);
+	~Bootstrap(void);
+
+	void Startup(const char* file);
+
+private:
+	void LoadConfig(const char* file);
+
+private:
+	rapidjson::Document _config;
+};
+
+#endif
