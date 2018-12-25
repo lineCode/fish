0ce4bea236cd2f6a5b4b0a3b132bd2b754225ef3
2109 <cai32109@gmail.com>
2018-12-25 19:46:39 +0800

    bug

--- 3rd/libev/build/cmake_install.cmake
+++ 3rd/libev/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/github/fish/lib/3rd/libev
+# Install script for directory: E:/github/fish/3rd/libev
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/github/fish/lib/3rd/libev/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/github/fish/3rd/libev/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
