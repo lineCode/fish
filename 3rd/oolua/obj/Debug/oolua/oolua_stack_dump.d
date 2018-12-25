75d856a18b5b1633bbca5578898ce68b2721e3a0
mrq <362651591@qq.com>
2018-12-25 23:14:40 +0800

    update

---
+++ 3rd/oolua/obj/Debug/oolua/oolua_stack_dump.d
@@ -0,0 +1,18 @@
+obj/Debug/oolua/oolua_stack_dump.o: src/oolua_stack_dump.cpp \
+ include/lua_includes.h ../lua/src/lua.h ../lua/src/luaconf.h \
+ ../lua/src/lauxlib.h ../lua/src/lua.h ../lua/src/lualib.h \
+ include/oolua_stack_dump.h
+
+include/lua_includes.h:
+
+../lua/src/lua.h:
+
+../lua/src/luaconf.h:
+
+../lua/src/lauxlib.h:
+
+../lua/src/lua.h:
+
+../lua/src/lualib.h:
+
+include/oolua_stack_dump.h:
--- hx/time/Timer.h
+++ hx/time/Timer.h
@@ -14,21 +14,21 @@ public:
 
 	void StartTimer(Network::EventPoller* poller, float after, float repeat)
 	{
-		io_.set(poller->GetEvLoop());
-		io_.set(after, repeat);
-		io_.set<Timer, &Timer::OnTimeout>(this);
-		io_.start();
-	}
-
-	void OnTimeout(ev::timer &watcher, int revents)
-	{
-		HandleTimeout();
+		timer_.set(poller->GetEvLoop());
+		timer_.set(after, repeat);
+		timer_.set<Timer, &Timer::OnTimeout>(this);
+		timer_.start();
 	}
 
 	virtual void HandleTimeout() = 0;
 
 private:
-	ev::timer io_;
+	void OnTimeout(ev::timer &watcher, int revents)
+	{
+		HandleTimeout();
+	}
+
+	ev::timer timer_;
 };
 
 #endif
