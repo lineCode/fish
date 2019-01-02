f674b369c23a3493fe066dea459187dbc78ada1c
2109 <cai32109@gmail.com>
2019-01-02 18:20:25 +0800

    asf

--- hx/Logger.cpp
+++ hx/Logger.cpp
@@ -10,14 +10,24 @@ Logger::Loglevel Logger::level_ = Trace;
 
 Logger::Logger(Addr& addr, ServerApp* app):addr_(addr) {
 	app_ = app;
+	
 	Network::Connector connector(app_->Poller());
 	int fd = connector.connect(addr_, false);
 	assert(fd > 0);
+
 	channel_ = new Network::Channel(app_->Poller(), fd);
 	channel_->SetCloseCallback(std::bind(&Logger::OnChannelClose, this, std::placeholders::_1))
+
+	timer_ = new Timer();
+	timer_->SetCallback(std::bind(&Logger::OnUpate, this, std::placeholders::_1, std::placeholders::_2));
+	timer_->Start(poller_, 1, 1);
 }
 
 Logger::~Logger(void) {
+	if (channel_) {
+		delete channel_;
+	}
+	delete timer_;
 }
 
 void Logger::Log(const char* file,int line,Loglevel level,const char* content) {
