67b5c4d9131f2b543b575bee762c28d83b7a25fe
2109 <cai2109@gmail.com>
2019-01-04 16:03:56 +0800

    update

--- hx/util/Util.cpp
+++ hx/util/Util.cpp
@@ -6,7 +6,6 @@ namespace Util {
 	int LoadJson(rapidjson::Document& doc, const char* file) {
 		FILE* F = fopen(file, "r");
 		if (!F) {
-			LOG_ERROR(fmt::format("LoadJson error:no such file:{}", file));
 			return -1;
 		}
 		
@@ -18,7 +17,6 @@ namespace Util {
 		fclose(F);
 
 		if (doc.HasParseError()) {
-			LOG_ERROR(fmt::format("LoadJson error:{}", doc.GetParseError()));
 			return -1;
 		}
 
