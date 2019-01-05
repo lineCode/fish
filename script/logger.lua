52704d39db96c382ea738f2d1e50e89962c8979a
2109 <cai32109@gmail.com>
2019-01-05 16:53:01 +0800

    update

--- script/logger.lua
+++ script/logger.lua
@@ -1,7 +1,29 @@
 
 
+local acceptor = nil
+
+
+
+local function OnData(channel, data, size)
+	local file, log = util.ParseLoggerMessage(data, size)
+	print(file,log)
+end
+
+local function OnClose(channel)
+
+end
+
+local function OnError(channel)
+
+end
+
+local function OnClientAccept(fd, addr)
+	fish.Bind(fd, 2, OnData, OnClose, OnError)
+end
+
 function Init()
 	print("logger init")
+	acceptor = fish.Listen("127.0.0.1", 1989)
 end
 
 function Fina()
