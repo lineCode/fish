adf9f5835f791bce23ea8268470fa68b8848fd92
2109 <cai32109@gmail.com>
2019-01-08 11:40:21 +0800

    bug

---
+++ test/PoolObject.h
@@ -0,0 +1,34 @@
+﻿#ifndef POOLOBJECT_H
+#define POOLOBJECT_H
+#include <assert.h>
+#include <stdio.h>
+#include "Allocator.h"
+
+template <class T, int ALLOC_COUNT = 64>
+class PoolObject
+{
+public:
+	static void* operator new(size_t size)
+	{
+		if (_allocator == NULL)
+			_allocator = new Allocator(T::GetClassName(), size, ALLOC_COUNT);
+		return _allocator->New();
+	}
+
+	static void	operator delete(void* obj)
+	{
+		_allocator->Delete(obj);
+	}
+
+	static int Dump() {
+		return _allocator->Dump();
+	}
+private:
+	static Allocator* _allocator;
+};
+
+
+template <class T, int ALLOC_COUNT>
+Allocator* PoolObject<T, ALLOC_COUNT>::_allocator = NULL;
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "Allocator.h"
+#include "PoolObject.h"
+
+struct TestA : public PoolObject<TestA> {
+	int a;
+
+	static std::string GetClassName() {
+		return std::string("TestA");
+	}
+};
+
+struct TestB : public PoolObject<TestB> {
+	int a;
+	int b;
+	static std::string GetClassName() {
+		return std::string("TestB");
+	}
+};
+
+struct TestC : public TestA {
+	int b;
+
+	static std::string GetClassName() {
+		return std::string("TestC");
+	}
+};
+int main() {
+
+	TestA* a = new TestA();
+	TestB* b = new TestB();
+	TestC* c = new TestC();
+
+	PoolObject<TestA>::Dump();
+	PoolObject<TestB>::Dump();
+	PoolObject<TestC>::Dump();
+	return 0;
+}
