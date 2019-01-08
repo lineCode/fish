22cb93e334944fc86d56df6138dd3c7a654c91b0
2109 <cai32109@gmail.com>
2019-01-08 13:29:33 +0800

    BUG

--- hx/util/PoolObject.h
+++ hx/util/PoolObject.h
@@ -9,23 +9,26 @@ class PoolObject
 public:
 	static void* operator new(size_t size) {
 		assert(sizeof( T ) == size);
-		return allocator_.New();
+		if (!allocator_) {
+			allocator_ = new Allocator(T::GetClassName(), sizeof( T ), ALLOC_COUNT);
+		}
+		return allocator_->New();
 	}
 
 	static void	operator delete(void* ptr) {
-		allocator_.Delete(ptr);
+		allocator_->Delete(ptr);
 	}
 
 	static void DumpPool() {
-		allocator_.Dump();
+		allocator_->Dump();
 	}
 
 private:
-	static Allocator allocator_;
+	static Allocator* allocator_;
 };
 
 
 template <class T, int ALLOC_COUNT>
-Allocator PoolObject<T, ALLOC_COUNT>::allocator_(T::GetClassName(), sizeof( T ), ALLOC_COUNT);
+Allocator* PoolObject<T, ALLOC_COUNT>::allocator_ = NULL;
 
 #endif
