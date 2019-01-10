d1946c95f8c0e98e787279c8e4a5fe9e722a8386
2109 <cai2109@gmail.com>
2019-01-10 17:11:09 +0800

    update

--- agent/AgentApp.cpp
+++ agent/AgentApp.cpp
@@ -22,8 +22,6 @@ int AgentApp::Init(rapidjson::Document& config) {
 	std::string boot("agent");
 	ServerApp::Init(boot);
 
-	lua_->DoFile("../../script/client.lua");
-	
 	if (!config.HasMember("clientAddr")) {
 		Util::Exit("no client addr");
 	}
--- script/agent.lua
+++ script/agent.lua
@@ -16,3 +16,16 @@ end
 function Update(now)
 
 end
+
+--call by cxx
+function _G.OnClientEnter(vid)
+	print("client enter",vid)
+end
+
+function _G.OnClientError(vid)
+	print("client error",vid)
+end
+
+function _G.OnClientData(vid, data, size)
+
+end
