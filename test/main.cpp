8bc688ac5709b623ad9feecc630a8affcf0f23d0
2109 <cai2109@gmail.com>
2019-01-17 16:37:06 +0800

    update

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,14 @@
 ﻿#include <unordered_map>
 #include <iostream>
-#include "TaskQueue.h"
+
+#include "logger/Logger.h"
+#include "logger/LoggerHost.h"
+#include "db/DbMysql.h"
+
 int main() {
-	
+	Logger* logger = new Logger(new LoggerHost("./"));
+	DbMysql* db = new DbMysql("127.0.0.1", 3306, "root", "123456");
+	db->Attach("test");		
 	
 	return 0;
 }
