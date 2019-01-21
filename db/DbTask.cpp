595a8003a3ebdda3c6db08a22905cef8c0696476
2109 <cai32109@gmail.com>
2019-01-21 19:10:20 +0800

    xx

--- db/DbTask.cpp
+++ db/DbTask.cpp
@@ -20,13 +20,13 @@ void DbQueryTask::ThreadDo(DbMysql* db) {
 }
 
 void DbQueryTask::MainDo() {
-	uint32_t nfields;
-	result_ >> nfields;
+	uint32_t fieldCount;
+	result_ >> fieldCount;
 
 	std::vector<std::string> names;
 	std::vector<uint32_t> types;
 
-	for (uint32_t i = 0;i < nfields;i++) {
+	for (uint32_t i = 0;i < fieldCount;i++) {
 		std::string name;
 		uint32_t type;
 		result_ >> name >> type;
@@ -34,16 +34,16 @@ void DbQueryTask::MainDo() {
 		types.push_back(type);
 	}
 
-	uint32_t nrows;
-	result_ >> nrows;
+	uint32_t rowCount;
+	result_ >> rowCount;
 
 	lua_State* L = APP->Lua()->GetScript().state();
 	lua_rawgeti(L, LUA_REGISTRYINDEX, reference_);
-	lua_createtable(L, nrows, 0);
+	lua_createtable(L, rowCount, 0);
 
-	for(uint32_t i = 0;i < nrows;i++) {
-		lua_createtable(L, nfields, 0);
-		for(uint32_t j = 0;j < nfields;j++) {
+	for(uint32_t i = 0;i < rowCount;i++) {
+		lua_createtable(L, fieldCount, 0);
+		for(uint32_t j = 0;j < fieldCount;j++) {
 			uint32_t length;
 			result_ >> length;
 			if (length == 0) {
@@ -80,7 +80,6 @@ void DbQueryTask::MainDo() {
 				}
 				case MYSQL_TYPE_NULL: {
 					continue;
-					break;
 				}
 				default: {
 					lua_pushlstring(L, data, length);
@@ -94,9 +93,12 @@ void DbQueryTask::MainDo() {
 
 		lua_rawseti(L, -2, i + 1);
 	}
+
 	if (LUA_OK != lua_pcall(L, 1, 0, 0)) {
-		LOG_ERROR(fmt::format("dbtask error"));
+		LOG_ERROR(fmt::format("DbQueryTask::MainDo error:{}", lua_tostring(L, -1)));
 	}
+
+	luaL_unref(L, LUA_REGISTRYINDEX, reference_);
 }
 
 DbRawSqlTask::DbRawSqlTask(int reference, const char* sql, size_t size) {
@@ -125,6 +127,8 @@ void DbRawSqlTask::MainDo() {
 	lua_pushinteger(L, affectedRows);
 
 	if (LUA_OK != lua_pcall(L, 1, 0, 0)) {
-		LOG_ERROR(fmt::format("DbRawSqlTask error:{}", lua_tostring(L, -1)));
+		LOG_ERROR(fmt::format("DbRawSqlTask::MainDo error:{}", lua_tostring(L, -1)));
 	}
+
+	luaL_unref(L, LUA_REGISTRYINDEX, reference_);
 }
