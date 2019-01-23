0a7439400f983dbe0f084fe14db1a5c7758eb5ba
2109 <cai32109@gmail.com>
2019-01-23 17:40:01 +0800

    xx

--- script/db.lua
+++ script/db.lua
@@ -4,12 +4,35 @@ local common = require "lib.common"
 
 function Init(self)
 	RUNTIME_LOG:ERROR("db init")
-	for i = 1,10000 do
-		db.Query("select * from user",function (result)
-		--	table.print(result)	
+	-- for i = 1,10000 do
+	-- 	db.Query("select * from user",function (result)
+	-- 	--	table.print(result)	
 		
+	-- 	end)
+	-- end
+
+	local CREATE_TABLE_ITEM = [[
+		CREATE TABLE IF NOT EXISTS `item`(
+		   `itemUid` INT UNSIGNED AUTO_INCREMENT,
+		   `userUid` INT UNSIGNED,
+		   `cfgId` INT UNSIGNED,
+		   `amount` INT UNSIGNED,
+		   PRIMARY KEY ( `itemUid` ),
+		   KEY `userUid` (`userUid`) USING BTREE
+		)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	]]
+
+	db.Execute(CREATE_TABLE_ITEM,function (result)
+		table.print(result)
+	end)
+
+	for i=1,100 do
+		local sql = string.format("insert into item (userUid,cfgId,amount) values (%d,%d,%d)",userUid,math.random(1,50),math.random(1,10))
+		db.Execute(sql,function (result)
+			table.print(result)
 		end)
 	end
+
 end
 
 function Fina()
