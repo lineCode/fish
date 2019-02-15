7f3f6483235903bd339e231a572c863acffa6398
2109 <cai32109@gmail.com>
2019-02-15 11:00:31 +0800

    xx

--- hx/logger/LoggerServer.cpp
+++ hx/logger/LoggerServer.cpp
@@ -52,7 +52,7 @@ void LoggerServer::WriteLog(const char* file, const char* source, int line, int 
 
 	fwrite(buff, n + len + 2, 1, F);
 	fflush(F);
-	
+
 	if (show_) {
 		fwrite(buff, n + len + 2, 1, stderr);
 	}
@@ -60,6 +60,19 @@ void LoggerServer::WriteLog(const char* file, const char* source, int line, int 
 	if (buff != stack) {
 		free(buff);
 	}
+
+	// std::string date = fmt::format("{}-{}-{} {}:{}:{}", stm.tm_year+1900, stm.tm_mon+1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec);
+	// if (decimal != 0) {
+	// 	date.append(fmt::format(".{}", decimal));
+	// }
+	
+	// std::string log = fmt::format("[{}][{} @{}:{}] {}\r\n", kLOG_TAG[level], date, source, line, content);
+
+	// fwrite(log.c_str(), log.length() , 1, F);
+	// fflush(F);
+	// if (show_) {
+	// 	fwrite(log.c_str(), log.length() , 1, stderr);
+	// }
 }
 
 void LoggerServer::WriteLog(const char* file, void* data, size_t size) {
