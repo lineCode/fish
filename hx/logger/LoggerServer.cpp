94c83a05a6f99bf2ad5af36951e70c289aa4549a
2109 <cai32109@gmail.com>
2019-02-15 10:58:10 +0800

    xx

--- hx/logger/LoggerServer.cpp
+++ hx/logger/LoggerServer.cpp
@@ -32,45 +32,33 @@ void LoggerServer::WriteLog(const char* file, const char* source, int line, int 
 
 	uint64_t decimal = (time - (time_t)time) * 1000;
 
-	// char date[64] = { 0 };
-	// if (decimal != 0) {
-	// 	snprintf(date, 64, "%d-%d-%d %d:%d:%d.%ld", stm.tm_year + 1900, stm.tm_mon + 1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec, decimal);
-	// } else {
-	// 	snprintf(date, 64, "%d-%d-%d %d:%d:%d", stm.tm_year + 1900, stm.tm_mon + 1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec);
-	// }
-	
-	// char stack[128] = { 0 };
-	// char* buff = stack;
-	// int n = snprintf(buff, 128, "[%c][%s @%s:%d]", kLOG_TAG[level], date, source, line);
-
-	// size_t len = strlen(content);
-	// if ( n + len >= 126 ) {
-	// 	buff = (char*)malloc(n + len + 2);
-	// }
-	// memcpy(buff + n, content, len);
-	// memcpy(buff + n + len, "\r\n", 2);
-
-	// fwrite(buff, n + len + 2, 1, F);
-	// fflush(F);
-	// // if ( show_ ) {
-	// // 	fwrite(buff, n + len + 2, 1, stderr);
-	// // }
-
-	// if (buff != stack) {
-	// 	free(buff);
-	// }
-
-	std::string date = fmt::format("{}-{}-{} {}:{}:{}", stm.tm_year+1900, stm.tm_mon+1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec);
+	char date[64] = { 0 };
 	if (decimal != 0) {
-		date.append(fmt::format(".{}", decimal));
+		snprintf(date, 64, "%d-%d-%d %d:%d:%d.%ld", stm.tm_year + 1900, stm.tm_mon + 1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec, decimal);
+	} else {
+		snprintf(date, 64, "%d-%d-%d %d:%d:%d", stm.tm_year + 1900, stm.tm_mon + 1, stm.tm_mday, stm.tm_hour, stm.tm_min, stm.tm_sec);
 	}
 	
-	std::string log = fmt::format("[{}][{} @{}:{}] {}\r\n", kLOG_TAG[level], date, source, line, content);
+	char stack[256] = { 0 };
+	char* buff = stack;
+	int n = snprintf(buff, 256, "[%c][%s @%s:%d]", kLOG_TAG[level], date, source, line);
 
-	fwrite(log.c_str(), log.length() , 1, F);
+	size_t len = strlen(content);
+	if ( n + len >= 254 ) {
+		buff = (char*)malloc(n + len + 2);
+	}
+	memcpy(buff + n, content, len);
+	memcpy(buff + n + len, "\r\n", 2);
+
+	fwrite(buff, n + len + 2, 1, F);
 	fflush(F);
+	
 	if (show_) {
-		fwrite(log.c_str(), log.length() , 1, stderr);
+		fwrite(buff, n + len + 2, 1, stderr);
+	}
+
+	if (buff != stack) {
+		free(buff);
 	}
 }
 
